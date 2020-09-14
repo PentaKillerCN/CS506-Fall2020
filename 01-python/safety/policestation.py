43168289ff0c971140cb1bdb100e6362de496952
PentaKillerCN <bidijzx@bu.edu>
2020-09-13 21:38:18 -0400

    Modified the built police station again. Now it supports variable height(default set as 20), and the increment for i has been resolved. A bug with height has also been resolved. Added a new line for the ground.

--- 01-python/safety/policestation.py
+++ 01-python/safety/policestation.py
@@ -1,3 +1,29 @@
-def draw_policestation():
-    print("police station not found")
+def draw_policestation(height=20):
+    print('      ---------------------------------------------')
+    print('      |                                           |      ')
+    print('      |               POLICE STATION              |      ')
+    i = 8
+    while(i<height):
+        if i%3 == 0:
+            if i + 4 > height:
+                while i < height:
+                    print('      |                                           |      ')
+                    i += 1
+            else:
+                print('      |      _____        _____        _____      |      ')
+                print('      |     |     |      |     |      |     |     |      ')
+                print('      |     |     |      |     |      |     |     |      ')
+                print('      |     |_____|      |_____|      |_____|     |      ')
+                i += 4
+        else:
+            print('      |                                           |      ')
+            i += 1
+    print('      |                                   ______  |      ')
+    print('      |                                  | BUPD | |      ')
+    print('      |         _____   _________        |______| |      ')
+    print('      |     ___|     | |    |    |       |        |      ')
+    print('      |    |_  ___  _| |    |    |       |        |      ')
+    print('      |      ()   ()   |    |    |       |        |      ')
+    print('---------------------------------------------------------\n')    
+
     return
