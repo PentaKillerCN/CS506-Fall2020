a0e7f2dd7f0c27d8b96aaf7d3df2cb1604831985
PentaKillerCN <bidijzx@bu.edu>
2020-09-21 19:50:33 -0400

    finished 02-library python exercise. Modified the parts that had problems. Removed the .DS_Store files that were accidentally uploaded.

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,25 @@
 def read_csv(csv_file_path):
-    """
-        Given a path to a csv file, return a matrix (list of lists)
-        in row major.
-    """
-    raise NotImplementedError()
+	"""
+	    Given a path to a csv file, return a matrix (list of lists)
+	    in row major.
+	"""
+	ret = []
+	file = open(csv_file_path, 'r')
+	lines = file.readlines()
+	for line in lines:
+		list_of_strings = line.split(',')
+		list_of_values = []
+		for item in list_of_strings:
+			try:
+				v = int(item)
+				list_of_values.append(v)
+			except ValueError:
+				try:
+					v = float(item)
+					list_of_values.append(v)
+				except ValueError:
+					v = item.strip("\"")
+					list_of_values.append(v)
+		ret.append(list_of_values)
+
+	return ret
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,56 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+	if not x or not y:
+		raise ValueError('lengths must not be zero')
+	if len(x) != len(y):
+		raise ValueError('lengths must be equal')
+	sum = 0
+	for i in range(len(x)):
+		sum += (x[i] - y[i])**2
+	return sum**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+	if not x or not y:
+		raise ValueError('lengths must not be zero')
+	if len(x) != len(y):
+		raise ValueError('lengths must be equal')
+	sum = 0
+	for i in range(len(x)):
+		if x[i] > y[i]:
+			sum += x[i] - y[i]
+		else:
+			sum += y[i] - x[i]
+	return sum
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+	if not x or not y:
+		raise ValueError('lengths must not be zero')
+	if len(x) != len(y):
+		raise ValueError('lengths must be equal')
+	diff = 0
+	score = 0
+	for i in range(len(x)):
+		if x[i] != y[i]:
+			diff += 1
+		else:
+			score += 1
+	return 1 - score/(score+diff)
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+	if not x or not y:
+		raise ValueError('lengths must not be zero')
+	if len(x) != len(y):
+		raise ValueError('lengths must be equal')
+	vec_product = 0
+	norm_x = 0
+	norm_y = 0
+	for i in range(len(x)):
+		vec_product += x[i] * y[i]
+		norm_x += x[i]**2
+		norm_y += y[i]**2
+	norm_product = norm_x**(1/2) * norm_y**(1/2)
+	if norm_product == 0:
+		raise ZeroDivisionError()
+	return vec_product/norm_product
+
 
 # Feel free to add more
